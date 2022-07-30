5f34bfa8623fc169a36a29cdcbcaa5a618c556cb
JC <nchan009@gmail.com>
2022-07-30 11:37:46 +1000

    change context path for social

--- src/main/java/au/com/jc/data/controller/SocialController.java
+++ src/main/java/au/com/jc/data/controller/SocialController.java
@@ -6,17 +6,15 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
 
+@CrossOrigin(origins= {"*"}, maxAge = 4800, allowCredentials = "false" )
 @Slf4j
 @RestController
-@RequestMapping("/api/v1")
+@RequestMapping("/api/v1/social")
 public class SocialController {
 
     @Autowired
